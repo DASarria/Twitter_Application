38e2f39cba0626feb355510ba7133cdef19bdb25
DASarria <163359984+DASarria@users.noreply.github.com>
2026-04-20 21:40:52 -0500

    Delete twitter_microservices_lambda directory

--- twitter_microservices_lambda/posts-service/src/main/java/co/edu/escuelaing/twitter/postsservice/Post.java
+++
@@ -1,9 +0,0 @@
-package co.edu.escuelaing.twitter.postsservice;
-
-public record Post(
-        String id,
-        String authorSub,
-        String authorName,
-        String content,
-        String createdAt) {
-}
--- twitter_microservices_lambda/stream-service/src/main/java/co/edu/escuelaing/twitter/streamservice/Post.java
+++
@@ -1,9 +0,0 @@
-package co.edu.escuelaing.twitter.streamservice;
-
-public record Post(
-        String id,
-        String authorSub,
-        String authorName,
-        String content,
-        String createdAt) {
-}
